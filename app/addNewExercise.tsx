588cb933dbe0e1d5de81157025dabffc66f5bcef
Dustin Pham <dustinpham07@gmail.com>
2026-02-05 13:34:37 -0800

    Added button for selection of days when adding a new exercise to workout

--- app/addNewExercise.tsx
+++ app/addNewExercise.tsx
@@ -10,14 +10,18 @@ import { Pressable,
   View,
   StyleSheet,
   TextInput,
-  ScrollView
+  ScrollView,
+  Button
+  
  } from "react-native";
 import { useEffect, useState } from "react";
 import { supabase } from "@/utils/supabase";
 export default function NewWorkout() {
   const [exerciseName, setExerciseName ] = useState("")
   const [listOfExercises, setListOfExercises] = useState([])
+  const [selectedDays, setSelectedDays] = useState([])
 
+  const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
   const exerciseTextBoxChange = (event: any) => {
     setExerciseName(event.target.value)
   }
@@ -41,6 +45,14 @@ export default function NewWorkout() {
     }
     fetchExerciseList()
   }, [exerciseName])
+  
+  const toggleDay = (day) => {
+    setSelectedDays((prev) =>
+      prev.includes(day)
+        ? prev.filter((d) => d !== day) // remove
+        : [...prev, day]                // add
+    )
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View>
@@ -49,18 +61,17 @@ export default function NewWorkout() {
         </Pressable>
       </View>
 
-      <View style={styles.container}>
-        <Text>Form here</Text>
+      <View style={styles.inputTextContainer}>
         <TextInput
-          placeholder="Exercise name"
+          placeholder="Input exercise name"
           onChange={exerciseTextBoxChange}
-          style={styles.inputTextContainer}
+          style={styles.inputText}
         />
       </View>
 
       <ScrollView 
         style={styles.scrollView}
-        showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={true}
         contentContainerStyle = {styles.scrollContent}
         >
           {listOfExercises.map((exercise) => (
@@ -68,6 +79,49 @@ export default function NewWorkout() {
           ))}
 
       </ScrollView>
+
+      <View style={styles.weekdayButtonsContainer}>
+        {weekdays.map((day, index) => {
+          const isSelected = selectedDays.includes(day)
+
+          return (
+          <Text>{day} 
+            <Pressable 
+              key={day}
+                style={() => 
+                [styles.button,
+                { 
+                  backgroundColor: isSelected ? 'green' : 'gray',
+                },
+                ]}
+              onPress={() => toggleDay(day)}
+            >
+              <Text> </Text>
+            </Pressable>
+          </Text>
+          )
+        }
+
+        ) }
+{/*         <Pressable style={() => 
+          [styles.button,
+          { 
+            backgroundColor: monPressed ? 'green' : 'gray',
+          },
+          ]}
+          onPress={handleButtonPressed}
+        >
+          <Text>Mon</Text>
+        </Pressable>
+        <Button title="Tue" />
+        <Button title="Wed" />
+        <Button title="Thu" />
+        <Button title="Fru" />
+        <Button title="Sat" />
+        <Button title="Sun" /> */}
+
+
+      </View>
     </SafeAreaView>
   )
 }
@@ -77,11 +131,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F8FAFC',
   },
-  inputTextContainer:{
+  inputTextContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputText:{
     backgroundColor: 'white',
     fontSize: 15,
     textAlign: 'center',
-    margin: 1,
+    margin: 15,
+    padding: 5,
+    width: '80%',
+    borderRadius: '10px',
   },
   scrollView: {
     flex: 1,
@@ -91,4 +153,17 @@ const styles = StyleSheet.create({
     paddingBottom: 100,
     flexGrow: 1
   },
+  weekdayButtonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  }
 })
